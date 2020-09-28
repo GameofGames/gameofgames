fdf710ec9e316d44a78182b7cc04ca6584cdb5db
Nicole <nicoleduu@gmail.com>
2020-09-28 14:58:42 -0400

    skeleton done

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+	entry: path.resolve('./client/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/build/',
+		filename: 'bundle.js'
+	},
+	mode: 'production',
+	devServer: {
+		contentBase: path.join(__dirname, 'client'),
+		publicPath: '/build/',
+		proxy: {
+			'/api': 'http://localhost:3000',
+		},
+		hot: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+}
